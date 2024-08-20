67d417cdd727cf3d73ac01c0e154cd63fc5d8074
Darien Hallam <darienhallam@gmail.com>
2024-08-20 12:36:20 +0100

    Initial Commit

---
+++ The Adventures of Pickle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Adventures of Pickle
+//
+//  Created by Darien Hallam on 20/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Adventures of Pickle/The_Adventures_of_PickleApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Adventures_of_PickleApp.swift
+//  The Adventures of Pickle
+//
+//  Created by Darien Hallam on 20/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct The_Adventures_of_PickleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
